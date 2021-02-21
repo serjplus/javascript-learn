ca1756d868318c9e2a74011a9d412251f1fd82d8
serjplus@hotmail.com <serjplus@hotmail.com>
2021-02-21 18:12:16 +0400

    Add Cycles.js to Project

---
+++ js/cycles.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let num = 50;
+while (num < 55) {
+   console.log(num);
+   num++; 
+}
+
+let numb = 50;
+do {
+   console.log(numb);
+   numb++ ; 
+}
+while (num < 55);
+
+for (let i = 1; i < 8; i++ ) {
+    console.log(i);
+}
