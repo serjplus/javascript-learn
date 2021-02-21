e8bd31e947a0e723baadf36cf5e925ced02b3069
serjplus <serjplus@hotmail.com>
2021-02-21 12:57:07 +0400

    Template files for learn JavaScript

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/*jslint es6 devel: true */
+
+let number = 5,
+    incr = 5,
+    decr = 5;
+
+const leftBorderWidth = 1;
+const  isCheckedVal1 = number;
+const  isCheckedVal2 = number;
+const  isCheckedVal3 = number;
+
+const answers = [];
+
+answers[0] = typeof( +prompt("Vvedite pervoe chislovoe znachenie", "0") );
+answers[1] = typeof( +prompt("Vvedite vtoroe chislovoe znachenie", "0") );
+answers[2] = typeof( +prompt("Vvedite tretee chislovoe znachenie", "0") );
+
+console.log(answers);
+console.log(answers[0] === isCheckedVal1 && answers[1] === isCheckedVal2 && answers[2] === isCheckedVal3);
